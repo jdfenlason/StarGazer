af0766e41cac25ff2ec7afedbb6df945194b3af5
Jake Fenlason <jdfenlason@gmail.com>
2021-04-13 16:39:46 +0000

    set up event listener for icon in footer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 /* eslint-disable no-console */
 var $zipCodeInput = document.querySelector('.zip-code');
+var $weatherHomeBtn = document.querySelector('#weather-home-button');
 
-$zipCodeInput.addEventListener('keypress', enterZip);
+$zipCodeInput.addEventListener('keyup', enterZip);
+$weatherHomeBtn.addEventListener('click', weatherHomeClick);
 
 function enterZip(event) {
   console.log(event.target.value);
+}
+
+function weatherHomeClick(event) {
+  console.log(event.target);
 }
