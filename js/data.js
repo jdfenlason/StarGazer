383f47e3c3133c4a3dedb8629227ce1ec242e5a2
Jake Fenlason <jdfenlason@gmail.com>
2021-04-13 18:40:04 +0000

    create data-set for API data and set up local storage

--- js/data.js
+++ js/data.js
@@ -1,1 +1,41 @@
 /* exported data */
+var observationData = {
+  observations: [],
+  editing: null,
+  nextObvId: 1
+};
+var astronomyData = {
+  zipCode: null
+};
+
+var weatherData = {
+  zipCode: null
+};
+
+var previousobservationDataJSON = localStorage.getItem('observationData-storage');
+var previousAstronomyDataJSON = localStorage.getItem('astronomyData-storage');
+var previousWeatherDataJSON = localStorage.getItem('astronomyData-storage');
+
+if (previousobservationDataJSON !== null) {
+  observationData = JSON.parse(previousobservationDataJSON);
+}
+
+if (previousAstronomyDataJSON !== null) {
+  astronomyData = JSON.parse(previousAstronomyDataJSON);
+}
+
+if (previousWeatherDataJSON !== null) {
+  weatherData = JSON.parse(previousWeatherDataJSON);
+}
+
+function storeData(event) {
+  var observationDataJSON = JSON.stringify(observationData);
+  localStorage.setItem('observationData-storage', observationDataJSON);
+  var astronomyDataJSON = JSON.stringify(astronomyData);
+  localStorage.setItem('astronomyData-storage', astronomyDataJSON);
+  var weatherDataJSON = JSON.stringify(weatherData);
+  localStorage.setItem('weatherData-storage', weatherDataJSON);
+
+}
+
+window.addEventListener('beforeunload', storeData);
